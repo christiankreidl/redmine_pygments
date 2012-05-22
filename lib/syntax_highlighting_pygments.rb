4dd1021229ab536bd660db34f11181e793170131
ichizok <gclient.gaap@gmail.com>
2012-05-22 18:37:57 +0900

    spacing

--- lib/syntax_highlighting_pygments.rb
+++ lib/syntax_highlighting_pygments.rb
@@ -11,10 +11,10 @@ module RedminePygments
         def highlight_by_language(text, language)
           content = ::Pygments.highlight(text, :lexer => language, :options => {:nowrap => true})
           lines = []
-          content.each_line {|line| lines << line }
+          content.each_line { |line| lines << line }
           index = "%##{lines.size.to_s.size}d"
-          lines.each_with_index {|e, i|
-            lines[i] = "<span class=\"line-numbers\">#{index % (i + 1)}</span>#{e}"
+          lines.each_with_index { |line, i|
+            lines[i] = "<span class=\"line-numbers\">#{index % (i + 1)}</span>#{line}"
           }
           "<div class=\"highlight\">#{lines.join}</div>"
         end
