b0375de149272c1e1dff666dd9bb0e06ec2aa1ab
ichizok <gclient.gaap@gmail.com>
2012-05-24 17:07:09 +0900

    modified description

--- init.rb
+++ init.rb
@@ -5,13 +5,13 @@ require 'syntax_highlighting_pygments'
 
 Rails.configuration.to_prepare do
   require_dependency 'application_helper'
-  ApplicationHelper.send(:include, PygmentsApplicationHelperPatch)
+  ApplicationHelper.send(:include, PygmentsApplicationHelperPatch) unless ApplicationHelper < PygmentsApplicationHelperPatch
 end
 
 Redmine::Plugin.register :redmine_pygments do
   name 'Redmine Pygments plugin'
   author 'ichizok'
-  description 'This is a plugin for Redmine'
+  description 'Syntax-highlighter powered by Pygments'
   version '0.0.1'
   url 'https://bitbucket.org/ichizok/redmine_pygments'
   author_url 'https://bitbucket.org/ichizok'
