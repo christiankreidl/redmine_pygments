861d37382fbb2e5d46ec2f4648aa3e019f7ae570
ichizok <gclient.gaap@gmail.com>
2012-05-30 19:05:36 +0900

    use String.rstrip

--- lib/syntax_highlighting_pygments.rb
+++ lib/syntax_highlighting_pygments.rb
@@ -11,14 +11,14 @@ module RedminePygments
         def highlight_by_language(text, language)
           opts = {:options => {:nowrap => true}}
           opts[:lexer] = language if ::Pygments::Lexer.find(language)
-          content = ::Pygments.highlight(text, opts)
+          hltext = ::Pygments.highlight(text, opts)
           lines = []
           i = 0
-          index = "%##{content.count("\n").to_s.size}d"
-          content.each_line { |line|
-            lines << "<span class=\"line-numbers\">#{index % (i += 1)}</span>#{line}"
-          }
-          "<div class=\"highlight\">#{lines.join[0 .. -2]}</div>"
+          idx = "%##{hltext.count("\n").to_s.size}d"
+          hltext.each_line do |line|
+            lines << "<span class=\"line-numbers\">#{idx % (i += 1)}</span>#{line}"
+          end
+          "<div class=\"highlight\">#{lines.join.rstrip}</div>"
         end
       end
     end
