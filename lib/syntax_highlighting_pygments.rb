8f63b54052100b24274ebef126269922ddbe158f
ichizok <gclient.gaap@gmail.com>
2013-02-06 22:04:20 +0900

    improved to be compatible with pygments.rb 0.3.x

--- lib/syntax_highlighting_pygments.rb
+++ lib/syntax_highlighting_pygments.rb
@@ -7,8 +7,10 @@ module Redmine
 
       class << self
         def highlight_by_filename(text, filename)
-          opts = {:filename => filename, :options => {:encoding => 'utf-8', :nowrap => true}}
           begin
+            opts = {:options => {:encoding => 'utf-8', :nowrap => true}}
+            lexer = ::Pygments.lexer_name_for(:filename => filename)
+            opts[:lexer] = lexer if lexer
             ::Pygments.highlight(text, opts)
           rescue
             ERB::Util.h(text)
@@ -16,10 +18,10 @@ module Redmine
         end
 
         def highlight_by_language(text, language)
-          opts = {:options => {:encoding => 'utf-8', :linenos => 'inline'}}
-          lexer = ::Pygments::Lexer.find(language)
-          opts[:lexer] = lexer[:aliases].first if lexer
           begin
+            opts = {:options => {:encoding => 'utf-8', :linenos => 'inline'}}
+            lexer = ::Pygments::Lexer.find(language)
+            opts[:lexer] = lexer[:aliases].first if lexer
             ::Pygments.highlight(text, opts).
               sub(%r{\A(<div class="highlight">)<pre>\n*}, '\1').
               sub(%r{\n*</pre>(</div>)\Z}, '\1')
